424a035beab42734738775397454f1d4172f4257
yasagts <yasa.gts@gmail.com>
2020-07-19 19:10:28 +0530

    added notes file

---
+++ ci_database/notes_ci_db.sql
@@ -0,0 +1,4 @@
+testGit/ci_database/test_db_ORCL.sql
+
+Running script C:\Program Files (x86)\Jenkins\workspace\Database-CI-Filescript\test_db_ORCL.sql on hr/********@ORCL
+Running script C:\Program Files (x86)\Jenkins\workspace\Database-CI-Filescript3\ci_database\test_db_ORCL.sql on hr/********@ORCL
